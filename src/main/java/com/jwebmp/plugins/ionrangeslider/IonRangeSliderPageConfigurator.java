b538d8ee4b4ef2da12297519aad172ad79e0a8ee
GedMarc <ged_marc@hotmail.com>
2019-02-09 11:52:38 +0200

    The Website API Brings out many things....

--- src/main/java/com/jwebmp/plugins/ionrangeslider/IonRangeSliderPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/ionrangeslider/IonRangeSliderPageConfigurator.java
@@ -88,7 +88,7 @@ public class IonRangeSliderPageConfigurator
 	@Override
 	public Page configure(Page page)
 	{
-		if (!page.isConfigured())
+		if (!page.isConfigured() && enabled())
 		{
 			JQueryPageConfigurator.setRequired(true);
 			page.getBody()
