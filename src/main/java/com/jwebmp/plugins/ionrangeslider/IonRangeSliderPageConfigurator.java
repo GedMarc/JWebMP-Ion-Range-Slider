beb86949a4b3baab5bf9c5b389f3e9c140e9665e
GedMarc <ged_marc@hotmail.com>
2020-12-17 09:02:44 +0200

    Pge configurator type safety and test class updates for it

--- src/main/java/com/jwebmp/plugins/ionrangeslider/IonRangeSliderPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/ionrangeslider/IonRangeSliderPageConfigurator.java
@@ -93,7 +93,7 @@ public class IonRangeSliderPageConfigurator
 
 	@NotNull
 	@Override
-	public Page configure(Page page)
+	 public Page<?> configure(Page<?> page)
 	{
 		if (!page.isConfigured() && enabled())
 		{
